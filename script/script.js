776c85d5473555d31f81abe845e98e1c0a3c0afd
paticarolina59 <paticarolina59@alumnos.cei.es>
2024-07-17 13:11:56 +0200

    entrega final

--- script/script.js
+++ script/script.js
@@ -237,3 +237,30 @@ let intervalo = setInterval(pasaImagen,3000);
 
 
 window.onload = handleEvent;
+
+
+// Animacion de secciones
+
+let boxes = document.querySelectorAll ('section');
+
+checkBoxes();
+
+function checkBoxes ()
+{
+    let triggerBotton = window.innerHeight / 5 * 4;
+
+    boxes.forEach((section) => {
+        let sectionTop = section.getBoundingClientRect().top;
+
+        if (sectionTop < triggerBotton)
+        {
+            section.classList.add('show')
+        }
+        else
+        {
+            section.classList.remove('show')
+        }
+    });
+}
+
+window.addEventListener('scroll', checkBoxes)
